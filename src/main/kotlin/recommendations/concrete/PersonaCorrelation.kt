f5201a470dcae7c3bdfa9cee9c4e53c5c1636a42
Alessandro Dicosola <alessandro.dicosola@outlook.it>
2019-07-31 13:16:28 +0200

    a few changes

--- src/main/kotlin/recommendations/concrete/PersonaCorrelation.kt
+++ src/main/kotlin/recommendations/concrete/PersonaCorrelation.kt
@@ -3,6 +3,8 @@ package recommendations.concrete
 import recommendations.skel.IWeightCalculator
 import recommendations.skel.Neighbor
 import recommendations.skel.User
+import java.util.logging.Logger
+import kotlin.math.log
 import kotlin.math.sqrt
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
@@ -17,42 +19,43 @@ import kotlin.test.assertTrue
  * @param ratingsV Rates given by V to items rated also by U
  */
 class PersonaCorrelation<Key>(
-    var ratingsU: Map<Key, Double>,
-    var ratingsV: Map<Key, Double>,
-    var user: User,
-    var neighbor: Neighbor
+    private val ratingsU: Map<Key, Double>,
+    private val ratingsV: Map<Key, Double>,
+    private val user: User,
+    private val neighbor: Neighbor
 ) :
     IWeightCalculator {
     /**
-     * The value calculated is used for setting [neighbor] weight
      * @return Weight between U and V
      */
     override fun calculate(): Double {
         assertEquals(ratingsU.size, ratingsV.size)
-
         assertTrue(ratingsU.all { ratingsV.containsKey(it.key) }, "ratingsU and ratingsV contains different keys")
+
+
 
         val normalizedU = ratingsU.mapValues { it.value - user.avg }
         val normalizedV = ratingsV.mapValues { it.value - neighbor.avg }
-        val den1 = normalizedU.mapValues { it.value * it.value }
-        val den2 = normalizedV.mapValues { it.value * it.value }
-        return normalizedU.mapValues { it.value * normalizedV.get(it.key)!! }.asSequence().sumByDouble { it.value }
-            .div(sqrt(den1.asSequence().sumByDouble { it.value }.times(den2.asSequence().sumByDouble { it.value })))
-            .let {
-                it
-            }
+
+        val num = normalizedU.mapValues { it.value * normalizedV[it.key]!! }.asSequence().sumByDouble { it.value }
+        val den1 = normalizedU.mapValues { it.value * it.value }.asSequence().sumByDouble { it.value }
+        val den2 = normalizedV.mapValues { it.value * it.value }.asSequence().sumByDouble { it.value }
+        val prodDen = sqrt(den1 * den2)
+        val result = num / prodDen
+
+
+        return result
     }
 
 }
 
 /**
- * The value calculated is used for setting [neighbor] weight
  * @return Weight between U and V
  */
 class WeightedPersonaCorrelation<Key>(
-    var ratingsU: Map<Key, Double>,
-    var ratingsV: Map<Key, Double>,
-    var user: User,
-    var neighbor: Neighbor,
+    ratingsU: Map<Key, Double>,
+    ratingsV: Map<Key, Double>,
+    user: User,
+    neighbor: Neighbor,
     factor: Int
 ) : WeightedCorrelation(PersonaCorrelation(ratingsU, ratingsV, user, neighbor), ratingsU.size, factor)
--- src/main/kotlin/recommendations/concrete/WeightedCorrelation.kt
+++ src/main/kotlin/recommendations/concrete/WeightedCorrelation.kt
@@ -4,8 +4,12 @@ import recommendations.skel.IWeightCalculator
 import kotlin.math.min
 
 //Use the number of items, used for calculating the weight, for normalize the weight in order to give more importance to weight obtained with a lot of items
-open class WeightedCorrelation(val weightCalculator: IWeightCalculator, val numberoOfItems: Int, val factor: Int) : IWeightCalculator {
+open class WeightedCorrelation(
+    private val weightCalculator: IWeightCalculator,
+    private val numberOfItems: Int,
+    private val factor: Int
+) : IWeightCalculator {
     override fun calculate(): Double {
-        return weightCalculator.calculate() * min(numberoOfItems, factor).toDouble() / factor
+        return weightCalculator.calculate() * (min(numberOfItems, factor) / factor).toDouble()
     }
 }
